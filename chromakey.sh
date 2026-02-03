eafdf2e835396ecfdf42e782e00f07ba034d7548
kolserdav <kolserdav@conhos.ru>
2026-02-03 14:49:33 +0700

    fixed chromakey

--- chromakey.sh
+++ chromakey.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-ffmpeg -hwaccel cuda -i output1.mp4 -i image1.png \
--filter_complex "[0:v]hwupload_cuda,chromakey_cuda=color=0x21914b:similarity=0.15:blend=0,hwdownload[ckout];[1:v]scale=1920:1080[bg];[bg][ckout]overlay" \
--c:v h264_nvenc -preset fast -c:a copy output_video1.mp4
+ffmpeg -hwaccel cuda -i input.mp4 -i image.png \
+ -filter_complex "[0:v]hwupload_cuda,chromakey_cuda=color=0x21914b:similarity=0.15:blend=0,hwdownload[ckout];[1:v]scale=1920:1080[bg];[bg][ckout]overlay" \
+ -c:v h264_nvenc -preset fast -b:v 22000k -b:a 192k output.mp4
