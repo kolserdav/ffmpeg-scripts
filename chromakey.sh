8328bf7051b42cd9d3f8d01849a89012b237b129
kolserdav <kolserdav@conhos.ru>
2026-02-04 18:34:02 +0700

    change bitrait

--- chromakey.sh
+++ chromakey.sh
@@ -2,4 +2,4 @@
 
 ffmpeg -hwaccel cuda -i input.mp4 -i image.png \
  -filter_complex "[0:v]hwupload_cuda,chromakey_cuda=color=0x21914b:similarity=0.15:blend=0,hwdownload[ckout];[1:v]scale=1920:1080[bg];[bg][ckout]overlay" \
- -c:v h264_nvenc -preset fast -b:v 22000k -b:a 192k output.mp4
+ -c:v h264_nvenc -preset fast -b:v 21222k -b:a 192k output.mp4
