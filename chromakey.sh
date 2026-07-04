7507a8ab18c8ff78e14597f2c69d7f914a66ed33
kolserdav <kolserdav@conhos.ru>
2026-07-04 17:31:34 +0700

    add .gitignore

--- chromakey.sh
+++ chromakey.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 ffmpeg -hwaccel cuda -i input.mp4 -i image.png \
- -filter_complex "[0:v]hwupload_cuda,chromakey_cuda=color=0x21914b:similarity=0.15:blend=0,hwdownload[ckout];[1:v]scale=1920:1080[bg];[bg][ckout]overlay" \
+ -filter_complex "[0:v]hwupload_cuda,chromakey_cuda=color=0x21914b:similarity=0.11:blend=0,hwdownload[ckout];[1:v]scale=1920:1080[bg];[bg][ckout]overlay" \
  -c:v h264_nvenc -preset fast -b:v 21222k -b:a 192k output.mp4
